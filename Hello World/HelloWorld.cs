a5f4caad8b5a5da3b31ef26d08ba988f7c8f0dbc
unknown <ssfu2@venus.ad.ncsa.edu>
2013-11-21 18:08:04 -0600

    Hello World completed

---
+++ Hello World/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Keep the console window open
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
