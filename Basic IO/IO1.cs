dd06cb407fddc8e210e6bad4fd63b2723412afa6
unknown <ssfu2@venus.ad.ncsa.edu>
2013-12-03 16:58:44 -0600

    Experimenting with very basic IO

---
+++ Basic IO/IO1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class IO1
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+        Console.ReadKey();
+	}
+}
---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a console application. How fancy!");
+            // Comments are very easy to use
+
+            // C# has neat aliases for its built-in variable types
+            // int is shorthand for System.Int32
+            bool yes = true;
+            if (yes)
+            {
+                Console.WriteLine(yes + ", indubitably.");
+            }
+
+            for (int i = 0; i < 5; i ++)
+            {
+                Console.WriteLine("Loops!");
+            }
+        }
+    }
+}
